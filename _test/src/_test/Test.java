1ee3a60ff7fc34600b09b1a0b8fc2ae502c3c173
Administrator <Administrator@I91T1FM30VWNVX5>
2020-01-03 11:54:24 +0800

    2

--- _test/src/_test/Test.java
+++ _test/src/_test/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		String string="123";
 		int i=10;
+		System.out.println(string);
+		System.out.println(i);
+		System.out.println(i+string);
 	}
 
 }
